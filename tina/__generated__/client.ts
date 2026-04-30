512894fe100c5efacad94462df39a08aa065b552
sheringugi <sheri.ngugi17@gmail.com>
2026-04-30 20:51:49 +0300

    force tina schema change trigger

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/AK/tajdo_ngo/tajdongo/tina/__generated__/.cache/1777571441717', url: 'https://content.tinajs.io/2.3/content/e9f7ba47-0078-4e62-8695-f561cd362cf8/github/main', token: 'f2c2dcb9567367f62d6471f8405dc6a635b1bb27', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/AK/tajdo_ngo/tajdongo/tina/__generated__/.cache/1777571497559', url: 'https://content.tinajs.io/2.3/content/e9f7ba47-0078-4e62-8695-f561cd362cf8/github/main', token: 'f2c2dcb9567367f62d6471f8405dc6a635b1bb27', queries,  });
 export default client;
   
