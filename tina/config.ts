bb294abc70185b53d287c24949fb52f65cfa8a4f
sheringugi <126586468+sheringugi@users.noreply.github.com>
2026-04-30 23:21:58 +0300

    Add clientId and token to Tina config

--- tina/config.ts
+++ tina/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tinacms";
 
 export default defineConfig({
   branch: process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || "main",
+  
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || process.env.VITE_TINA_CLIENT_ID || "",
   token: process.env.TINA_TOKEN || process.env.VITE_TINA_TOKEN || "",
   build: {
