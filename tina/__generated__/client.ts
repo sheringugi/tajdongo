73ac84e816cdbcd08009edf459090eae4ece32cc
sheringugi <sheri.ngugi17@gmail.com>
2026-05-01 05:14:51 +0300

    fix corrupted tina config

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/AK/tajdo_ngo/tajdongo/tina/__generated__/.cache/1777600441449', url: 'https://content.tinajs.io/2.3/content/e9f7ba47-0078-4e62-8695-f561cd362cf8/github/main', token: 'd4c92508afa27b40c4416eb680252be1a4e68b60', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/AK/tajdo_ngo/tajdongo/tina/__generated__/.cache/1777601468764', url: 'https://content.tinajs.io/2.3/content/e9f7ba47-0078-4e62-8695-f561cd362cf8/github/main', token: 'd4c92508afa27b40c4416eb680252be1a4e68b60', queries,  });
 export default client;
   
